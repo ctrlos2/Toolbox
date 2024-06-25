3f6c65d2c54c353a18a4106a1375105b4c643eb6
ctrlos2 <161234397+ctrlos2@users.noreply.github.com>
2024-06-25 10:43:32 +0200

    Update 22222.ps1

--- print/22222.ps1
+++ print/22222.ps1
@@ -23,6 +23,10 @@ $form.Text = "Custom Launcher"
 $form.Size = New-Object System.Drawing.Size(800, 600)
 $form.StartPosition = "CenterScreen"
 
+# Tworzenie ikony aplikacji
+$icon = [System.Drawing.Icon]::ExtractAssociatedIcon((Get-Process -Id $PID).Path)
+$form.Icon = $icon
+
 # Tworzenie kontrolki TabControl
 $tabControl = New-Object System.Windows.Forms.TabControl
 $tabControl.Dock = [System.Windows.Forms.DockStyle]::Fill
@@ -31,16 +35,13 @@ $tabControl.Dock = [System.Windows.Forms.DockStyle]::Fill
 $tabPage1 = New-Object System.Windows.Forms.TabPage
 $tabPage1.Text = "Apps"
 
-# Funkcja do dodawania przycisków bez ikon
-function Add-ButtonToTab {
-    param (
-        [System.Windows.Forms.TabPage]$tabPage,
-        [string]$text,
-        [int]$top,
-        [scriptblock]$onClick
-    )
+# Funkcja do dodawania przycisków z ikonami
+function Add-ButtonToTab($tabPage, $text, $top, $icon, $onClick) {
     $button = New-Object System.Windows.Forms.Button
     $button.Text = $text
+    $button.Image = $icon
+    $button.ImageAlign = "MiddleLeft"
+    $button.TextImageRelation = "ImageBeforeText"
     $button.Size = New-Object System.Drawing.Size(250, 40)
     $button.Location = New-Object System.Drawing.Point(30, $top)
     $button.FlatStyle = "Flat"
@@ -49,33 +50,35 @@ function Add-ButtonToTab {
     $tabPage.Controls.Add($button)
 }
 
+# Ikony dla przycisków
+$icon1 = [System.Drawing.Icon]::ExtractAssociatedIcon("C:\temp\IPv4NetworkScan.ps1")
+$icon2 = [System.Drawing.Icon]::ExtractAssociatedIcon("C:\temp\nowyfolder.ps1")
+$icon3 = [System.Drawing.Icon]::ExtractAssociatedIcon("C:\temp\udostepnione_foldery_ip.ps1")
+$icon4 = [System.Drawing.Icon]::ExtractAssociatedIcon("C:\temp\udostepnione_foldery_host.ps1")
+$icon5 = [System.Drawing.Icon]::ExtractAssociatedIcon("C:\temp\ustawieniaudostepniania.ps1")
+
 # Akcje dla przycisków
-$action1 = { Start-Process -FilePath ".\IPv4NetworkScan.ps1" }
-$action2 = { Start-Process -FilePath ".\nowyfolder.ps1" }
-$action3 = { Start-Process -FilePath ".\udostepnione_foldery_ip.ps1" }
-$action4 = { Start-Process -FilePath ".\udostepnione_foldery_host.ps1" }
-$action5 = { Start-Process -FilePath ".\ustawieniaudostepniania.ps1" }
+$action1 = { Start-Process -FilePath "C:\temp\IPv4NetworkScan.ps1" }
+$action2 = { Start-Process -FilePath "C:\temp\nowyfolder.ps1" }
+$action3 = { Start-Process -FilePath "C:\temp\udostepnione_foldery_ip.ps1" }
+$action4 = { Start-Process -FilePath "C:\temp\udostepnione_foldery_host.ps1" }
+$action5 = { Start-Process -FilePath "C:\temp\ustawieniaudostepniania.ps1" }
 
 # Dodawanie przycisków do pierwszej zakładki
-Add-ButtonToTab -tabPage $tabPage1 -text "SKANOWANIE IP" -top 20 -onClick $action1
-Add-ButtonToTab -tabPage $tabPage1 -text "Nowy folder na dysku C" -top 80 -onClick $action2
-Add-ButtonToTab -tabPage $tabPage1 -text "Sprawdź Udostępnione IP" -top 140 -onClick $action3
-Add-ButtonToTab -tabPage $tabPage1 -text "Udostępnione foldery host" -top 200 -onClick $action4
-Add-ButtonToTab -tabPage $tabPage1 -text "Ustawienia Udostępniania" -top 260 -onClick $action5
-
-# Dodanie zakładki do kontrolki TabControl
-$tabControl.TabPages.Add($tabPage1)
-
-# Dodanie kontrolki TabControl do formularza
-$form.Controls.Add($tabControl)
-
-# Wyświetlanie formularza
-$form.ShowDialog()
+Add-ButtonToTab $tabPage1 "SKANOWANIE IP" 20 $icon1 $action1
+Add-ButtonToTab $tabPage1 "Nowy folder na dysku C" 80 $icon2 $action2
+Add-ButtonToTab $tabPage1 "Sprawdź Udostępnione IP" 140 $icon3 $action3
+Add-ButtonToTab $tabPage1 "Udostępnione foldery host" 200 $icon4 $action4
+Add-ButtonToTab $tabPage1 "Ustawienia Udostępniania" 260 $icon5 $action5
 
 # ==================== Tab Page 2 ====================
 $tabPage2 = New-Object System.Windows.Forms.TabPage
 $tabPage2.Text = "Directories"
 
+# Dodawanie przycisku "Starter" z ikoną
+$iconStarter = [System.Drawing.Icon]::ExtractAssociatedIcon(".\PowerLauncher64.exe")
+$actionStarter = { Start-Process ".\PowerLauncher64.exe" }
+Add-ButtonToTab $tabPage2 "Uruchom PowerLauncher64.exe" 20 $iconStarter $actionStarter
 
 # Dodawanie zakładek do TabControl
 $tabControl.TabPages.Add($tabPage1)
